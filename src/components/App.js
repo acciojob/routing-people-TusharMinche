0b149696fb69c45fdaa3ff9810aa3b6efb19c850
TusharMinche <tusharminche@gmail.com>
2025-09-13 23:36:06 +0530

    Fixed async/await error

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,18 @@ import UserList from "./UserList";
 import UserDetails from "./UserDetails";
 import { Routes, Route } from "react-router-dom";
 import axios from "axios";
+import "regenerator-runtime/runtime";
 
 const App = () => {
   const [users, setUsers] = useState([]);
 
-  const fetchUsers = () => {
-    const response = axios.get(
-      "https://jsonplaceholder.typicode.com/users/"
-    ).then(response => setUsers(response.data))
-    .catch(err => console.error(err));;
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/users/");
+      setUsers(response.data)
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   useEffect(() => {
@@ -22,9 +25,9 @@ const App = () => {
   return (
     <div>
       <Routes>
-          <Route path='/' element={<UserList users={users}/>}/>
-          <Route path='/users/:id' element={<UserDetails />}/>
-        </Routes>
+        <Route path="/" element={<UserList users={users} />} />
+        <Route path="/users/:id" element={<UserDetails />} />
+      </Routes>
     </div>
   );
 };
--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,29 +1,40 @@
 import React, {useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from "axios";
+import "regenerator-runtime/runtime";
 
 const UserDetails = () => {
-    const {id} = useParams();
-
-    const [user, setUser] = useState(null);
-
-    const fetchUser = () => {
-    const response = axios.get(
-      `https://jsonplaceholder.typicode.com/users/${id}`
-    ).then(response => setUser(response.data))
-    .catch(err => console.error(err));
-  };
+  const {id} = useParams();
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
+    const fetchUser = async () => {
+      setUser(null);
+      try {
+        const response = await axios.get(
+          `https://jsonplaceholder.typicode.com/users/${id}`
+        );
+        setUser(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
     fetchUser();
-  }, []);
+  }, [id]);
+
   return (
     <div>
-        {user === null ? <div>Loading...</div> : <p>
-                Name: {user.name}, Username: {user.username}, Email: {user.email}, Phone: {user.phone}, Website: {user.website}
-            </p>}
+      {user === null ? (
+        <div>Loading...</div>
+      ) : (
+        <p>
+          Name: {user.name}, Username: {user.username}, Email: {user.email}, 
+          Phone: {user.phone}, Website: {user.website}
+        </p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default UserDetails
+export default UserDetails;
